e71376ed820bc156d67d435705719d0bc3dcdbb6
Floaty_KT <FloatKasemtan@users.noreply.github.com>
2022-08-10 16:33:04 +0700

    chore: delete unused variable

--- kasemtan-portfolio/src/pages/Projects.tsx
+++ kasemtan-portfolio/src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Loading } from "../components/Loading";
 import { ProjectCards } from "../components/Projects/ProjectCards";
 import "../styles/projects.scss";
