cedb60f75f388e419152b1abcff0e040b37b6f45
Floaty_KT <FloatKasemtan@users.noreply.github.com>
2022-08-13 23:47:11 +0700

    feat: change styling

--- kasemtan-portfolio/src/components/Projects/ProjectCards.tsx
+++ kasemtan-portfolio/src/components/Projects/ProjectCards.tsx
@@ -1,16 +1,27 @@
 import { Repository } from "../../types/repository";
 
 export const ProjectCards = (projectsData: Repository[]) => {
+  console.log(projectsData[0]);
+
   return (
-    <div className="grid gap-4 my-5 sm:grid-cols-2 md:grid-cols-3">
+    <div
+      style={{
+        padding: "2rem",
+        columnWidth: "320px",
+        maxWidth: "1100px",
+        margin: "0 auto",
+        columnFill: "unset",
+      }}
+    >
       {projectsData.map((project, i) => (
         <div
           key={project.id}
           style={{ animationDelay: `${0.05 * i}s` }}
-          className="project grid-item"
+          className="project grid-item flex flex-col justify-between"
         >
           <div className="text-2xl">{project.name}</div>
-          <div className="text-base">{project.description}</div>
+          <div className="text-base ">{project.description}</div>
+          <div className="self-end text-xs">{project.language}</div>
         </div>
       ))}
     </div>
