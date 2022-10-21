35b957c5f6a1b5c1df5b339a219e1766c4c813b2
Floaty_KT <FloatKasemtan@users.noreply.github.com>
2022-10-21 18:23:45 +0700

    change design

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -10,39 +10,39 @@ const Contacts: React.FC = () => {
   }, []);
 
   return (
-    <div className="px-48 pb-10" style={{}}>
-      <div className="flex-center font-header topic mb-10">Contact</div>
+    <div className="px-48 pb-10">
+      <div className="mb-10 text-xl flex-center topic">Contact</div>
       <div className="flex flex-col items-center gap-10">
         <a
-          className="flex p-1 gap-2 rounded-md border-[1px] border-gray-500"
+          className="flex p-2 gap-2 rounded-md border-[1px] border-gray-500 justify-center w-96 relative"
           href="mailto: work@floatykt.com"
           target="_blank"
         >
-          <img src={mailLogo} alt="Mail" />
+          <img className="absolute left-4" src={mailLogo} alt="Mail" />
           <div>work@floatykt.com</div>
         </a>
         <a
-          className="flex p-1 gap-2 rounded-md border-[1px] border-gray-500"
+          className="flex p-2 gap-2 rounded-md border-[1px] border-gray-500 justify-center w-96 relative"
           href="https://github.com/FloatKasemtan"
           target="_blank"
         >
-          <img src={githubLogo} alt="Github" />
+          <img className="absolute left-4" src={githubLogo} alt="Github" />
           <div>FloatKasemtan</div>
         </a>
         <a
-          className="flex p-1 gap-2 rounded-md border-[1px] border-gray-500"
+          className="flex p-2 gap-2 rounded-md border-[1px] border-gray-500 justify-center w-96 relative"
           href="https://www.facebook.com/float.teavasirichokchai/"
           target="_blank"
         >
-          <img src={facebookLogo} alt="Facebook" />
+          <img className="absolute left-4" src={facebookLogo} alt="Facebook" />
           <div>(Float) Kasemtan Tevasirichokchai</div>
         </a>
         <a
-          className="flex p-1 gap-2 rounded-md border-[1px] border-gray-500"
+          className="flex p-2 gap-2 rounded-md border-[1px] border-gray-500 justify-center w-96 relative"
           href="https://www.linkedin.com/in/kasemtan-teavasirichokchai-975531227/"
           target="_blank"
         >
-          <img src={linkedinLogo} alt="Linkedin" />
+          <img className="absolute left-4" src={linkedinLogo} alt="Linkedin" />
           <div>Kasemtan Tevasirichokchai</div>
         </a>
       </div>
